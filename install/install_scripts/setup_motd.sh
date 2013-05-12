f04bfadb47c72c6e448b26286f0328526e89dfd0
Tim Perry <tim.r.perry@gmail.com>
2013-05-12 15:56:39 +0100

    Fixed install

--- install/install_scripts/setup_motd.sh
+++ install/install_scripts/setup_motd.sh
@@ -5,5 +5,6 @@ sudo chmod -x /etc/update-motd.d/*
 
 echo -e "Installing climan motd\n";
 sudo cp /usr/lib/climan/install/template_files/00-climan /etc/update-motd.d/
+sudo chmod +x /etc/update-motd.d/00-climan
 
 echo -e "Done.\n\n";
