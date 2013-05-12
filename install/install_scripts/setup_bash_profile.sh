57c6c174c99d41bcb893bbe635a7384d637dae73
Tim Perry <tim.r.perry@gmail.com>
2013-05-12 23:41:07 +0100

    Permission updates

--- install/install_scripts/setup_bash_profile.sh
+++ install/install_scripts/setup_bash_profile.sh
@@ -6,4 +6,6 @@ echo -e "Setting up bash profile (enables use of util functions)\n";
 
 sudo cp /usr/lib/climan/install/template_files/.bash_profile /root
 
+chmod -R +x /usr/lib/climan/bin
+
 echo -e "Done.\n\n";
