4147dc81ca9d5281daafddbab6a39ba47276dc4b
Tim Perry <tim.r.perry@gmail.com>
2013-05-13 20:06:12 +0100

    Fixed acl for package managers

--- install/install_scripts/setup_acls.sh
+++ install/install_scripts/setup_acls.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-echo -e "Setting up ACL's\n";
-
 # delete existing rules
 iptables -F
 iptables -X
@@ -24,37 +22,29 @@ iptables -A OUTPUT -o lo -j ACCEPT
 
 # ssh port 2020
 iptables -A INPUT -p tcp -s 172.16.20.1 --dport 2020 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 2020 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 2020 -j ACCEPT
 
-# http / https
-iptables -A INPUT -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
+# http
+iptables -A INPUT -p tcp --sport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 
-# http / https
+# https
 iptables -A INPUT -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
-
-# ping
-iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
-iptables -A OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 443 -j ACCEPT
 
 # smtp
 # possibly setup smtps (http://www.cyberciti.biz/faq/howto-configure-sendmail-ssl-certificate-email/)
-iptables -A INPUT -p tcp --dport 25 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 25 -m state --state ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 25 -j ACCEPT
+iptables -A INPUT -p tcp --dport 25 -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp --sport 25 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 25 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 25 -j ACCEPT
 
 # dns
-iptables -A INPUT -p udp --sport 53 -j ACCEPT
-iptables -A INPUT -p tcp --sport 53  -j ACCEPT
+iptables -A INPUT -p udp --sport 53 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --sport 53 -m state --state ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
 
-# apt-get
-#todo!!
-
 # prevent dos
 iptables -A INPUT -p tcp --dport 80 -m limit --limit 25/minute --limit-burst 100 -j ACCEPT
-
-echo -e "Done.\n\n";
